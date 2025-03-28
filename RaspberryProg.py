7b30bf877148655c43e0a64dc6ade393998a04b1
Axel-S1 <bj.axelsichi@gmail.com>
2025-03-29 00:46:48 +0100

    New Branch

--- RaspberryProg.py
+++ RaspberryProg.py
@@ -1,5 +1,7 @@
 from lidar.MyLidarClass import MyLidar
 from wifi_com.ServeurClass import Server
+import pygame
+import serial
 
 from flask import jsonify
 
@@ -12,6 +14,51 @@ class Robot(Server):
     def take_Lidar(self):
         data = self.lidar.getScanData(360, format=1)
         return jsonify(data.tolist())
+    
+def main():
+    pygame.init()
+    pygame.joystick.init()
+    
+    arduino = serial.Serial('COM3', 9600)
+    
+    if pygame.joystick.get_count() == 0:
+        print("Aucune manette détectée.")
+        return
+    
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    
+    print(f"Manette détectée: {joystick.get_name()}")
+    
+    running = True
+    status = b'S'
+    oldstatus = b'S'
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        axesX = joystick.get_axis(1)
+        axesY = joystick.get_axis(0)
+        
+        if   axesX <= -0.9:
+            status = (b"B")
+        elif axesX >= 0.9:
+            status = (b"F")
+        elif axesY <= -0.9:
+            status = (b"R")
+        elif axesY >= 0.9:
+            status = (b"L")
+        else :
+            status = (b"S")
+        
+        if status != oldstatus:
+            print(status)
+            arduino.write(status)
+            oldstatus = status
+
+    pygame.quit()
+
     
 if __name__ == '__main__':    
     # Création d'un client et récupération du message
