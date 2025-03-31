2aedb14ee6b133d0912d9e5716ed7a79aef53e82
Axel.S <bj.axelsichi@gmail.com>
2025-03-31 15:55:26 +0200

    Interface 1V

---
+++ Interface/TraductionClass.py
@@ -0,0 +1,23 @@
+import json
+
+class Traduction:
+    def __init__(self, langue="fr"):
+        self.langue = langue
+        self.traductions = self.charger_traductions()
+
+    def charger_traductions(self):
+        try:
+            with open("Interface/traduction.json", "r", encoding="utf-8") as fichier:
+                return json.load(fichier)
+        except FileNotFoundError:
+            print("Fichier de traduction non trouvé !")
+            return {}
+
+    def traduire(self, cle):
+        return self.traductions.get(self.langue, {}).get(cle, cle)
+
+    def changer_langue(self, nouvelle_langue):
+        if nouvelle_langue in self.traductions:
+            self.langue = nouvelle_langue
+        else:
+            print(f"Langue '{nouvelle_langue}' non disponible !")
