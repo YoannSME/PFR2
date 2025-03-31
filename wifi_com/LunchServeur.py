514e7cdc3b11c69030610af97d978d6923baf573
Axel.S <bj.axelsichi@gmail.com>
2025-03-31 11:14:31 +0200

    Creation de la branche

--- wifi_com/LunchServeur.py
+++
@@ -1,8 +0,0 @@
-from ServeurClass import Server
-
-if __name__ == '__main__':
-    # Création et démarrage du serveur Flask
-    server = Server()
-    print(f"Serveur démarré sur {server.host}:{server.port}")
-    server.run()
-    
