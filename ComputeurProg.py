390d30ed2a0fa3a3ac7e5f82aa357877db7574fe
Axel.S <bj.axelsichi@gmail.com>
2025-04-03 15:22:56 +0200

    Pour le léo

--- ComputeurProg.py
+++ ComputeurProg.py
@@ -8,7 +8,7 @@ def Deplacement():
     pygame.init()
     pygame.joystick.init()
     
-    arduino = serial.Serial('COM5', 9600)
+    arduino = serial.Serial('COM6', 9600)
     
     if pygame.joystick.get_count() == 0:
         print("Aucune manette détectée.")
@@ -18,10 +18,9 @@ def Deplacement():
     joystick.init()
     
     print(f"Manette détectée: {joystick.get_name()}")
-    
+    cpt = 0
     running = True
     status = b'S'
-    oldstatus = b'S'
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,33 +30,33 @@ def Deplacement():
         axesY = joystick.get_axis(0)
         PressA = joystick.get_button(0)
         
-        if   axesX <= -0.9:
-            status = (b"B")
-        elif axesX >= 0.9:
+        if   axesX <= -0.5:
             status = (b"F")
-        elif axesY <= -0.9:
-            status = (b"R")
-        elif axesY >= 0.9:
+        elif axesX >= 0.5:
+            status = (b"B")
+        elif axesY <= -0.5:
             status = (b"L")
+        elif axesY >= 0.5:
+            status = (b"R")
         else :
             status = (b"S")
-            
         
-        if status != oldstatus:
+        cpt += 1
+        if cpt%5000 == 0:
             print(status)
             arduino.write(status)
-            oldstatus = status
 
-        if PressA:
-            message = client.json_to_numpy(client.getFromServeur('take_Lidar'))
-            cartographie.update_carte(message, ploting=True)  
+
+        # if PressA:
+        #     message = client.json_to_numpy(client.getFromServeur('take_Lidar'))
+        #     cartographie.update_carte(message, ploting=True)  
         
         
     pygame.quit()
 
 if __name__ == '__main__':    
-    # Création d'un client et récupération du message
-    client = Client('192.168.0.36')  # Remplace par l'IP du Raspberry Pi
-    cartographie = Cartographie()
+    # # Création d'un client et récupération du message
+    # client = Client('192.168.0.36')  # Remplace par l'IP du Raspberry Pi
+    # cartographie = Cartographie()
     Deplacement()
         
