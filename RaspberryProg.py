71bf27347568939f66b07f11e1ee7315e32144c7
M4rt3h <martin.leteurtre@univ-tlse3.fr>
2025-05-06 14:50:12 +0200

    Modif pour avoir l'image dans le bon sens (rotation 180°)

--- RaspberryProg.py
+++ RaspberryProg.py
@@ -3,13 +3,14 @@ from wifi_com.ServeurClass import Server
 from flask import send_file, abort, jsonify
 from picamera2 import Picamera2
 import os
+import cv2
+import numpy as np
 
 class Robot(Server):
     def __init__(self, LidarPort = '/dev/ttyUSB0'):
         super().__init__()
         self.lidar = MyLidar(LidarPort)
-        #Initialisation de la camera
-        self.picam2 = Picamera2()
+        self.picam2 = Picamera2() #Init de la cam
         self.picam2.start()
 
         self.run()
@@ -18,16 +19,21 @@ class Robot(Server):
         data = self.lidar.getScanData(360, format=1)
         return jsonify(data.tolist())
 
-    def take_picture(self):    
+def take_picture(self):
         image_path = "/home/gp3/Desktop/Images/imageRequete.jpg"
-        #Prendre une photo
-        self.picam2.capture_file("/home/gp3/Desktop/Images/imageRequete.jpg")
+        self.picam2.capture_file(image_path) #Prendre une photo
 
-        if os.path.exists(image_path):
-            return send_file(image_path, mimetype='image/jpg')
-        else :
-            return abort(404,description = "Image non trouvée")
-        
+        image = cv2.imread(image_path) #Charger image avec OpenCV
+
+        if image is not None:
+            rotated_image = cv2.rotate(image, cv2.ROTATE_180) #Et la faire tourner de 180°
+            rotated_image_path = "/home/gp3/Desktop/Images/imageRequete_rotated.jpg"
+            cv2.imwrite(rotated_image_path, rotated_image)
+
+            # Retourner l'image pivotée
+            return send_file(rotated_image_path, mimetype='image/jpg')
+        else:
+            return abort(404, description="Image non trouvée")       
         
     
     
